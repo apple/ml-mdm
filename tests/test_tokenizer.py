7147d66785cf557ff61e7204d5f3cdbc304cb73e
Pedro Cruz <pedroborgespc@gmail.com>
2024-11-14 15:59:47 -0500

    update test_tokenizer and fixed import in factory.py

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,32 +1,34 @@
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2024 Apple Inc. All rights reserved.
-from ml_mdm.language_models import tokenizer
+
+import logging
+
+# Tokenizer class from tokenizer.py
+from ml_mdm.language_models.tokenizer import Tokenizer # Tokenizer class from tokenizer.py
 
 # Vocab files are in /ml-mdm/data/
-# Difference between .vocab file to a token file? 
 
-# Tokenizer in factory.py create_tokenizer(vocab_file): you pass a vocab file, 
-#   function returns something (dictionary? a tokenizer?)
 # Tokenizer.py: each function is a different mode of tokenizing a token_file (?)
 #   (is this the same as a vocab file or is it a dictionary of tokens?) and they
 #   return a prefix tree structure.
 
-# CASE 1: We compare factory.py's create_tokenizer() to tokenizer.py's output.
-#  Ex.: assert create_tokenizer() == read_dictionary_bert()
-
-# CASE 2: We run the output of factory, and then pass its output as the token 
-#   file to tokenizer.py's functions. Then, we need something else to assert 
-#   against.
-#  Ex.: read_dictionary_bert(create_tokenizer(.vocab)) == ?
 
 def test_tokenizer_bert():
-    pass
+    f = "../data/bert.vocab" 
+    assert Tokenizer(f, mode="bert")
+    #Q: should we assert the contents of tokenizer?
 
 def test_tokenizer_t5():
-    pass
+    f = "../data/t5.vocab" 
+    assert Tokenizer(f, mode="tf")
+    #Q: should we assert the contents of tokenizer?
 
-# Is this a general vocab file that's not either bert or t5?
+# any vocab file that's not either bert or t5
 def test_tokenizer():
-    pass
+    f = "../data/imagenet.vocab" 
+    assert Tokenizer(f)
+    #Q: should we assert the contents of tokenizer?
 
-
+test_tokenizer_bert()
+test_tokenizer_t5()
+test_tokenizer()
