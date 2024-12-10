b4984a0a57babb0ccc309f151a64ca58f2998a29
Emma Garrett <egarret1@swarthmore.edu>
2024-12-09 23:01:25 -0500

    added command line arguments

--- ml_mdm/language_models/call_mlx_lm.py
+++ ml_mdm/language_models/call_mlx_lm.py
@@ -1,8 +1,9 @@
 from typing import Optional
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import argparse
 
 def call_mlx_lm(input_text: str, model_name: Optional[str] = "google/flan-t5-base") -> str:
-    tokenizer = T5Tokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
     input_ids = tokenizer.encode(input_text, return_tensors="pt")
@@ -10,3 +11,13 @@ def call_mlx_lm(input_text: str, model_name: Optional[str] = "google/flan-t5-bas
 
     output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
     return output_text
+
+# command line arguments
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_text", type=str, required=True)
+    parser.add_argument("--model", type=str, default="google/flan-t5-base")
+    args = parser.parse_args()
+
+    output_text = call_mlx_lm(args.input_text, args.model)
+    print(output_text)
