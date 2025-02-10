6e5ed07b5e5ae3e90d106408d8ac0c34a271a304
Bella Deanhardt <ideanha1@swarthmore.edu>
2025-02-10 10:29:23 -0800

    ResNet bug

--- ml_mdm/models/unet_mlx.py
+++ ml_mdm/models/unet_mlx.py
@@ -1,8 +1,12 @@
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2024 Apple Inc. All rights reserved.
 
+import einops
+
 import mlx.core as mx
 import mlx.nn as nn
+
+from ml_mdm.models.unet import ResNetConfig
 
 
 def zero_module_mlx(module):
@@ -32,3 +36,54 @@ class MLP_MLX(nn.Module):  # mlx based nn.Module
 
     def forward(self, x):
         return x + self.main(x)
+
+
+class ResNet_MLX(nn.Module):
+    def __init__(self, time_emb_channels, config: ResNetConfig):
+        # TODO(ndjaitly): What about scales of weights.
+        super(ResNet_MLX, self).__init__()
+        self.config = config
+        self.norm1 = nn.GroupNorm(config.num_groups_norm, config.num_channels)
+        self.conv1 = nn.Conv2d(
+            config.num_channels,
+            config.output_channels,
+            kernel_size=3,
+            padding=1,
+            bias=True,
+        )
+        self.time_layer = nn.Linear(time_emb_channels, config.output_channels * 2)
+        self.norm2 = nn.GroupNorm(config.num_groups_norm, config.output_channels)
+        self.dropout = nn.Dropout(config.dropout)
+        self.conv2 = zero_module_mlx(
+            nn.Conv2d(
+                config.output_channels,
+                config.output_channels,
+                kernel_size=3,
+                padding=1,
+                bias=True,
+            )
+        )
+        if self.config.output_channels != self.config.num_channels:
+            self.conv3 = nn.Conv2d(
+                config.num_channels, config.output_channels, kernel_size=1, bias=True
+            )
+
+    def forward(self, x, temb):
+        h = nn.silu(self.norm1(x))
+        h = self.conv1(h)
+        ta, tb = (
+            self.time_layer(nn.silu(temb)).unsqueeze(-1).unsqueeze(-1).chunk(2, dim=1)
+        )
+        if h.size(0) > ta.size(0):  # HACK. repeat to match the shape.
+            N = h.size(0) // ta.size(0)
+            ta = einops.repeat(ta, "b c h w -> (b n) c h w", n=N)
+            tb = einops.repeat(tb, "b c h w -> (b n) c h w", n=N)
+        h = nn.silu(self.norm2(h) * (1 + ta) + tb)
+        h = self.dropout(h)
+        h = self.conv2(h)
+        if self.config.output_channels != self.config.num_channels:
+            x = self.conv3(x)
+        return h + x
+
+    def __call__(self, x, temb):
+        return self.forward(x, temb)
--- tests/test_mlx_unet.py
+++ tests/test_mlx_unet.py
@@ -5,8 +5,8 @@ import mlx.core as mx
 import numpy as np
 import torch
 
-from ml_mdm.models.unet import MLP
-from ml_mdm.models.unet_mlx import MLP_MLX
+from ml_mdm.models.unet import MLP, ResNet, ResNetConfig
+from ml_mdm.models.unet_mlx import MLP_MLX, ResNet_MLX
 
 
 def test_pytorch_mlp():
@@ -56,3 +56,52 @@ def test_pytorch_mlp():
     ), "Outputs of PyTorch MLP and MLX MLP should match"
 
     print("Test passed for both PyTorch and MLX MLP!")
+
+
+def test_pytorch_ResNet():
+    """
+    Simple test for our ResNet implementations
+    """
+    # Define parameters
+    batch_size = 2
+    time_emb_channels = 32
+    height = 16
+    width = 16
+
+    # Create config
+    config = ResNetConfig(
+        num_channels=64,
+        output_channels=128,
+        num_groups_norm=32,
+        dropout=0.0,  # Set to 0 for deterministic comparison
+        use_attention_ffn=False,
+    )
+
+    # Create model instances
+    pytorch_resnet = ResNet(time_emb_channels=time_emb_channels, config=config)
+    mlx_resnet = ResNet_MLX(time_emb_channels=time_emb_channels, config=config)
+
+    # Set both models to evaluation mode
+    pytorch_resnet.eval()
+    mlx_resnet.eval()
+
+    # Create a dummy pytorch input tensor (batch size = 2, channels = 64, height, width = 16)
+    x_torch = torch.randn(batch_size, config.num_channels, height, width)
+    temb_torch = torch.randn(batch_size, time_emb_channels)
+
+    # pass the input thorugh the model
+    output_torch, activations_torch = pytorch_resnet(x_torch, temb_torch)
+
+    # Convert inputs to MLX tensors
+    x_mlx = mx.array(x_torch.numpy())
+    temb_mlx = mx.array(temb_torch.numpy())
+
+    # Get MLX output
+    output_mlx, activations_mlx = mlx_resnet(x_mlx, temb_mlx)
+
+    # Verify outputs match
+    assert np.allclose(
+        output_torch.detach().numpy(), np.array(output_mlx), atol=1e-5
+    ), "PyTorch and MLX ResNet outputs should match"
+
+    print("Test passed for ResNet implementations!")
